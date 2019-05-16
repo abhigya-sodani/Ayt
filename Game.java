c2ad8aa2d17558bb169f31b5591895d75deae3d7
tonygotchutney <50649600+tonygotchutney@users.noreply.github.com>
2019-05-16 08:23:04 -0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import java.util.*;
-import java.io.*;
 
 public class Game {
 	
@@ -21,13 +20,14 @@ public class Game {
 	JButton startButton, choice1, choice2, choice3, choice4;
 	JTextArea mainTextArea;
 	int clout , respect, cash;
-	String readiness , position;
+	String readiness , position, position2;
 	
 	TitleScreenHandler tsHandler = new TitleScreenHandler();
 	ChoiceHandler choiceHandler = new ChoiceHandler();
 	
 	JTextField txtInput = new JTextField("");
 	
+	int difficulty = 0;
 
 
 
@@ -176,6 +176,13 @@ public class Game {
 		mainTextArea.setText("Wacko Swami: Yo that right there is Symere. He’s the guy you want to talk to if you want to join Ayt Squad. He’s the manager and the lead rapper.\n" + 
 				"You: Yo bet can you introduce us, I sent them a demo a couple days ago.\n" + 
 				"Wacko Swami: Yooo Symere this my boy who can rap, he sent you that demo a couple days ago. \n" + "");
+		try {
+			ausdio a=new ausdio("Beginning(Sumeet).wav");
+			a.play();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
 		choice1.setText("Continue");
 		choice2.setText("Continue");
 		choice3.setText("Continue");
@@ -207,7 +214,7 @@ public class Game {
 	}
 	public void ifAChoiceOne(){
 		position = "ifAChoiceOne";
-		
+		position2 = "ruckers";
 		mainTextArea.setText("Symere: Yo this fire fam. You should come into the studio with us one day. ayt squad could use another member.\n" + 
 				"You: Yea, I’ll hit the stu witchu tmrw. I’m tryna get some battle rap in right now though. You down?  \n" + 
 				"Symere: No doubt, I’ll beat you.\n" + "");
@@ -219,7 +226,7 @@ public class Game {
 	}
 	public void ifAChoiceTwo(){
 		position = "ifAChoiceTwo";
-		
+		position2 = "ruckers";
 		mainTextArea.setText("Symere: Ima ignore that slight cause yo demo fire. You should hit the studio with us one day. \n" + 
 				"You: Yo how about this, we battle right here and If I win you let me join ayt squad.\n" + 
 				"Symere: Hahah you think you can beat ME? Bet\n" + "");
@@ -239,7 +246,7 @@ public class Game {
 	}
 	public void ifBChoiceOne(){
 		position = "ifBChoiceOne";
-		
+		position2 = "ruckers";
 		mainTextArea.setText("Symere: Dawg get outta here. You tryna catch this fade huh I’ll bet a band right now that you can’t take me in a cypher. \n" + 
 				"You: Bet let’s do it right here right now.\n" + 
 				"Symere: Bet if you win I’ll even let you join Ayt Squad.\n" + "");
@@ -259,7 +266,7 @@ public class Game {
 	}
 	public void ifBChoiceTwo(){
 		position = "ifBChoiceTwo";
-		
+		position2 = "ruckers";
 		mainTextArea.setText("Symere: Yo this is aight but idk how much of it is you and how much of it is ya boy.\n" + 
 				"You: How about this, lemme just hit you with this freestyle real quick. One band  says I can out rap you. \n" + 
 				"Symere: Hahah you think you can beat ME? Bet if you win I’ll even let you join Ayt Squad.\n" + "");
@@ -279,7 +286,8 @@ public class Game {
 	}
 	
 	public void battle(){
-		    
+	    
+		difficulty = 10;
 		mainTextArea.setText("Battle Stage");
 		position = "battle";
 		int count = 0;
@@ -314,12 +322,82 @@ public class Game {
 		}	
 }
 	
-	public void continued(){
-		position = "continued";
-		
-		mainTextArea.setText("The Next Day");			
-		
+	public void ruckers(){	
+		position = "ruckers";
+		position2 = "ruckers";
+		try {
+			ausdio a=new ausdio("");
+			a.play();
+			}
+			catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
+		mainTextArea.setText("The Next Day /nSymere: Dawg… you been for real killin’ it out there in the underground. \n" + 
+				"Player: Word? I’m tryna keep it low key.\n" + 
+				"Symere: Listen up fam. You got talent. You legit. \n" + 
+				"Player: Shoot thanks bro, but what’s the deal with this?\n" + 
+				"Symere: There’s this freestyle rap battle tournament coming up in a minute…\n" + 
+				"Player (Interrupting): Naw fam… that’s not me you know I’m not about that.\n" + 
+				"Symere: I think you’re up for it dawg. You our best rapper by a long shot. And I know you ain’t been doing too hot back at home. Your moms just lost her job so you need the money. The prize pot is 10 grand. That’s some life changing stuff and you know it.\n" + 
+				"Player: ... aight. I’m down… I guess.\n" + 
+				"Symere: That’s my dawg. Better start grinding. Show up to Rucker’s Park in a month. i’ll let you know the time soon. Until then practice and get better. Wacko knows a few people who can help you out.  \n" + 
+				"Player: I appreciate it man.\n" + 
+				"");	
+		choice1.setText("Talk to Wacko");
+		choice2.setText("Practice Battle");
+		choice3.setText("Quit");
+		choice4.setText("");
 	}
+	
+	public void training(){
+		position = "training";
+		position2 = "training";
+		mainTextArea.setText("You: Yo wacko, I heard you know some freestyle legends who can help me out. I’m tryna win this tournament at Ruckers coming up.\n" + 
+				"Wacko: Yea man my pops and his homies done won that tourney back in the day. If you can beat them you can beat anyone.\n" + 
+				"You: Yo can you introduce us? What’s your dads name?\n" + 
+				"Wacko: His name?  Mc Juice.\n");	
+		choice1.setText("Battle MC Juice");
+		choice2.setText("Practice Battle");
+		choice3.setText("Quit");
+		choice4.setText("");
+	}
+	
+	public void mcjuice(){
+		position = "mcjuice";
+		difficulty = 15;
+		int count = 0;
+		int losses = 0;
+		int wins = 0;
+		String input;
+		while(count<15) {
+			String b=RhymeGame.getRandomWord();
+			RhymeGame a=new RhymeGame(b);
+
+			JOptionPane.showMessageDialog(null, b);
+			a.getArray();
+			ArrayList rhymes=a.rhymes;
+			input = JOptionPane.showInputDialog("input");
+			if(rhymes.contains(input.toLowerCase().replaceAll(" ","" ).replaceAll("'","").replaceAll("-", ""))) {
+				wins++;
+				count++;	
+				JOptionPane.showMessageDialog(null, "Thats a rhyme! \n Attempts: " + count + "\n # Correct: " + wins +"\n # Wrong: " + losses);	
+			}
+			else {
+				losses++;
+				count++;
+				JOptionPane.showMessageDialog(null, "Lmao Nope! \n Attempts: " + count + "\n # Correct: " + wins +"\n # Wrong: " + losses);
+				
+			}
+	}
+		if(wins>losses) {
+			win();
+		}
+		else {
+			lose();
+		}	
+}
+
+	
 	public void replayIntroduction(){
 		position = "replayIntroduction";
 		
@@ -327,8 +405,12 @@ public class Game {
 	}
 	public void replayBattle(){
 		position = "replayBattle";
-		
-		battle();		
+		if(difficulty == 10) {
+			battle(); 
+		}
+		if(difficulty == 15) {
+			training();
+		}		
 	}
 	public void quit(){
 		position = "quit";
@@ -339,7 +421,7 @@ public class Game {
 	public void win(){
 		position = "win";
 		
-		mainTextArea.setText("");	
+		mainTextArea.setText("YOU WON");	
 		
 		clout+=15; 
 		cloutLabelNumber.setText(""+clout); 
@@ -350,8 +432,8 @@ public class Game {
 			
 		
 		choice1.setText("Continue");
-		choice2.setText("Quit");
-		choice3.setText("");
+		choice2.setText("Replay Battle");
+		choice3.setText("Quit");
 		choice4.setText("");
 		
 	}
@@ -367,9 +449,12 @@ public class Game {
 		mainTextArea.setText("YOU LOST . . .");
 		
 		choice1.setText("Replay Battle");
-		choice2.setText("Quit");
-		choice3.setText("");
+		choice2.setText("Replay Introduction");
+		choice3.setText("Quit");
 		choice4.setText("");
+	}	
+	public void continued(){
+		position = position2;
 	}	
 
 	public class TitleScreenHandler implements ActionListener{
@@ -390,7 +475,7 @@ public class Game {
 				switch(yourChoice){
 				case "c1": replayIntroduction(); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Symere (Intro).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -399,7 +484,7 @@ public class Game {
 				break;
 				case "c2": replayIntroduction(); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Symere (Intro).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -408,7 +493,7 @@ public class Game {
 				break;
 				case "c3": replayIntroduction(); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Symere (Intro).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -417,7 +502,7 @@ public class Game {
 				break;
 				case "c4": replayIntroduction(); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Symere (Intro).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -436,7 +521,14 @@ public class Game {
 				cash+=15; 
 				cashLabelNumber.setText(""+cash); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Sumeet (A).wav");
+					a.play();
+					}
+					catch(Exception e) {
+						System.out.println(e.getMessage());
+					}
+				try {
+					ausdio a=new ausdio("Symere (If A).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -451,7 +543,7 @@ public class Game {
 				cash+=15; 
 				cashLabelNumber.setText(""+cash); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Sumeet (B).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -470,7 +562,7 @@ public class Game {
 				cash+=15; 
 				cashLabelNumber.setText(""+cash); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Sumeet (If A Option 1).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -485,7 +577,7 @@ public class Game {
 				cash+=15; 
 				cashLabelNumber.setText(""+cash); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Sumeet (If A Option 2).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -504,7 +596,7 @@ public class Game {
 				cash+=15; 
 				cashLabelNumber.setText(""+cash); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Sumeet (If B Option 1).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -519,7 +611,7 @@ public class Game {
 				cash+=15; 
 				cashLabelNumber.setText(""+cash); 
 				try {
-					ausdio a=new ausdio("");
+					ausdio a=new ausdio("Sumeet (If B Option 2).wav");
 					a.play();
 					}
 					catch(Exception e) {
@@ -563,16 +655,42 @@ public class Game {
 			case "lose":
 				switch(yourChoice){
 				case "c1": replayBattle(); break;
-				case "c2": quit(); break;
+				case "c2": replayIntroduction(); break;
+				case "c3": quit(); break;
 				}
 				break;
 			case "win":
 				switch(yourChoice){
 				case "c1": continued(); break;
-				case "c2": quit(); break;
+				case "c2": replayBattle();break;
+				case "c3": quit(); break;
+				}
+				break;
+			case "ruckers":
+				switch(yourChoice){
+				case "c1": training(); 
+				try {
+					ausdio a=new ausdio("Training Conversation(Sumeet)");
+					a.play();
+					}
+					catch(Exception e) {
+						System.out.println(e.getMessage());
+					}
+				break;
+				case "c2": replayBattle(); break;
+				case "c3": quit(); break;
+				}
+				break;
+			case "training":
+				switch(yourChoice){
+				case "c1": mcjuice(); break;
+				case "c2": replayBattle(); break;
+				case "c3": quit(); break;
 				}
 				break;
 			}
 		}	
 	}
 }
+
+
