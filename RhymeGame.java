1993799b3f4b2b495d62eca59ca00f72b5fa375d
abhigya-sodani <37461794+abhigya-sodani@users.noreply.github.com>
2019-05-16 09:15:38 -0700

    Update RhymeGame.java

--- RhymeGame.java
+++ RhymeGame.java
@@ -7,7 +7,7 @@ import java.io.*;
 
 public class RhymeGame{
 	String word;
-	
+	 ArrayList<String> words=new ArrayList<String>();
 	ArrayList<String> rhymes=new ArrayList<String>();
 	public RhymeGame(String w) {
 		word=w;
@@ -37,7 +37,72 @@ public class RhymeGame{
 			System.out.println(e.getMessage());
 		}
 	}
-	
+	private void merge(ArrayList<String> a,int first,int mid,int last){
+		ArrayList<String> b= new ArrayList<String>(a.size());
+		   int c=mid;
+   int d=first;
+   while(c<=last && d<mid){
+    
+     if(a.get(d).length()>a.get(c).length()){
+         b.add(a.get(c));
+        
+         c++;
+         
+      }
+   else{
+         b.add(a.get(d));
+         
+         d++;
+         
+      }
+       
+     }
+		while(d<mid){
+			b.add(a.get(d++));
+			
+		}
+		while(c<=last){
+			b.add(a.get(c++));
+			
+		}
+		for(int o=first;o<=last;o++){
+			a.set(o,b.get(o-first));
+			
+		}
+	}
+
+
+  /**
+   *  Recursive mergesort of an array of integers
+   *
+   * @param  a      reference to an array of integers to be sorted
+   * @param  first  starting index of range of values to be sorted
+   * @param  last   ending index of range of values to be sorted
+   */
+  public void mergeSort(ArrayList <String> a, int first, int last){
+	//Add your code here
+	   if(last>first){
+	      
+	      int m=first+(last-first)/2;
+	      mergeSort(a,first,m);
+	      mergeSort(a,m+1,last);
+	      merge(a,first,m+1,last);
+	   }
+  }
+  public void sortWords() {
+	 
+	  try {
+	  	Scanner in=new Scanner(new File("words.txt"));
+	  	for(int i=0; i<10; i++) {
+	  		words.add(in.next());
+		  
+	  	}
+	  	mergeSort(words,0,words.size());
+	  }
+	  catch(Exception e) {
+		  System.out.println(e.getMessage());
+	  }
+  }
 	public static String getRandomWord() {
 		String word="";
 		try {
